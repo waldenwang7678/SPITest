d53076fe5da28b5907f349cf19a34b398db6ad62
wangjitao <767884538@qq.com>
2017-08-16 15:18:06 +0800

    done

--- app/src/main/java/com/wangjt/spitest/MainActivity.java
+++ app/src/main/java/com/wangjt/spitest/MainActivity.java
@@ -1,13 +1,47 @@
 package com.wangjt.spitest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.wangjt.aninterface.Display;
+
+import java.util.Iterator;
+import java.util.ServiceLoader;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        et = (TextView) findViewById(R.id.et_spi);
+        findViewById(R.id.bt_spi).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadMoudle();
+            }
+        });
+    }
+
+    private void loadMoudle() {
+        StringBuilder builder = new StringBuilder();
+        ServiceLoader<Display> loader = ServiceLoader.load(Display.class);
+        Iterator<Display> iterator = loader.iterator();
+        while (iterator.hasNext()) {
+            builder.append(iterator.next().display());
+        }
+
+        for (Display display : loader) {
+            builder.append(display.display()+"\n");
+        }
+        et.setText(builder.toString());
     }
 }
