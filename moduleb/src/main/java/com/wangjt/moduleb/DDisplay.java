318cb3d53b4e93e9dafa2e228f68ee044768a2fb
wangjitao <767884538@qq.com>
2017-11-16 17:26:37 +0800

    3333

---
+++ moduleb/src/main/java/com/wangjt/moduleb/DDisplay.java
@@ -0,0 +1,15 @@
+package com.wangjt.moduleb;
+
+import com.wangjt.aninterface.Display;
+
+/**
+ * @author wangjt
+ * @date 2017/11/16
+ */
+
+public class DDisplay extends Display {
+    @Override
+    public String display() {
+        return "this is D Display";
+    }
+}
